d1abcdeb5a43f6c990f45f718d88fb1bae48ffb2
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-26 02:54:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(placement)
   placement_array = []
   if placement.length == 0 
     puts "The line is currently empty."
-  elsif current_line_array.each_with_index[1] do |name,new_index| 
+  elsif current_line_array.each_with_index do |name,new_index| 
     placement_array.push("#{new_index}. #{name}")
   end
   puts "The line is: #{current_line_array.join(" ")}"
