f8b84d1b75cd7ad77c64443c375913351e885f25
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-26 02:49:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(placement)
+  placement_array = []
+  if placement.length == 0 
+    puts "The line is currently empty."
+  elsif current_line_array.each_with_index[1] do |name,new_index| 
+    placement_array.push("#{new_index}. #{name}")
+  end
+  puts "The line is: #{current_line_array.join(" ")}"
+end 
